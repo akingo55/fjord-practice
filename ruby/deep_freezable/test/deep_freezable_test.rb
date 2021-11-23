9123dd7c562d16b05eb18bfa4bd97f4f7862893f
akingo55 <am1157154@gmail.com>
2021-11-23 16:57:46 +0900

    update

--- ruby/deep_freezable/test/deep_freezable_test.rb
+++ ruby/deep_freezable/test/deep_freezable_test.rb
@@ -13,4 +13,18 @@ class DeepFreezableTest < Minitest::Test
     # 配列の要素がすべてfreezeされているか？
     assert Team::COUNTRIES.all? { |country| country.frozen? }
   end
+
+  def test_deep_freezable_to_hash
+    # hashの値は正しいか
+    assert_equal(
+      { 'Japan' => 'yen', 'US' => 'dollar', 'India' => 'rupee' },
+      Bank::CURRENCIES
+    )
+
+    # ハッシュ自身がfreezeされているか
+    assert Bank::CURRENCIES.frozen?
+
+    # hashの要素はすべてfreezeされているか
+    assert Bank::CURRENCIES.all? { |key,value| key.frozen? && value.frozen? }
+  end
 end
