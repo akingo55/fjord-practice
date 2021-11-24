cf31757445b33489064b6cef47764f2d5edc594a
akingo55 <am1157154@gmail.com>
2021-11-24 23:03:43 +0900

    add

---
+++ ruby/tempo/lib/tempo.rb
@@ -0,0 +1,34 @@
+class Tempo
+  include Comparable
+
+  attr_reader :bpm
+
+  def initialize(bpm)
+    @bpm = bpm
+  end
+
+  def <=>(other)
+    if other.is_a?(Tempo)
+      bpm <=> other.bpm
+    else
+      nil
+    end
+  end
+
+  def inspect
+    "#{bpm}bpm"
+  end
+end
+
+t_120 = Tempo.new(120)
+t_180 = Tempo.new(180)
+p t_120
+p t_180
+
+t_120 > t_180
+t_120 <= t_180
+t_120 == t_180
+
+tempos = [Tempo.new(180),Tempo.new(60),Tempo.new(120)]
+
+p tempos.sort
